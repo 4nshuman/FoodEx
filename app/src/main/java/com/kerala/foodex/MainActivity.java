c925e16cb1fd3ecdacc4418b29e651fa3ad9f4e1
Anshuman Tripathi <Anshuman01.Tripathi@gmail.com>
2020-03-19 20:26:42 +0530

    designed the start page

--- app/src/main/java/com/kerala/foodex/MainActivity.java
+++ app/src/main/java/com/kerala/foodex/MainActivity.java
@@ -3,12 +3,33 @@ package com.kerala.foodex;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button btnSignUp, btnLogIn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnSignUp = (Button)findViewById(R.id.signUp);
+        btnLogIn = (Button)findViewById(R.id.logIn);
+
+        btnSignUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        btnLogIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
     }
 }
